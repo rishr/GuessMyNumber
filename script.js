9624d1f9c6acef939dc6a687c0051838e99199f1
rishr <shrivastva.r@husky.neu.edu>
2020-12-14 11:34:20 -0500

    adding files

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'correct Number';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+
+
+// we wan to listen to the event
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
+
+document.querySelector('.check').addEventListener
+    ('click', function () {
+        const guess = Number(document.querySelector
+            ('.guess').value);
+        console.log(typeof guess);
+
+        if (!guess) {
+            document.querySelector('.message').textContent
+                = 'No Number';
+
+        } else if (guess === secretNumber) {
+            document.querySelector('.message')
+                .textContent = 'correct Number';
+
+        }
+
+        // when the guess is greater than the number
+        else if (guess > secretNumber) {
+
+            if (score > 1) {
+                document.querySelector('.message')
+                    .textContent = 'Yo! too High';
+                score--;
+                document.querySelector('.score')
+                    .textContent = score;
+            } else {
+                document.querySelector('.message')
+                    .textContent = 'Hey! Looser';
+                document.querySelector('.score')
+                    .textContent = 0;
+            }
+
+
+
+        } else if (guess < secretNumber) {
+            if (score > 1) {
+                document.querySelector('.message')
+                    .textContent = 'Yo! Low low low';
+                score--;
+                document.querySelector('.score')
+                    .textContent = score;
+            } else {
+                document.querySelector('.message')
+                    .textContent = 'Hey! Looser';
+                document.querySelector('.score')
+                    .textContent = 0;
+            }
+
+            // document.querySelector('.message')
+            //     .textContent = 'Yo! Low low low';
+            // score--;
+            // document.querySelector('.score')
+            //     .textContent = score;
+        }
+
+    });
+
+
+
